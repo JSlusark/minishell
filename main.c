0c2cd21890ea827de7863c829d63e59c31138214
jslusark <slusark.jessica@gmail.com>
2024-11-25 12:51:40 +0100

    cleaned up main and added small notes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jslusark <jslusark@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:26:03 by jslusark          #+#    #+#             */
-/*   Updated: 2024/11/25 11:46:16 by jslusark         ###   ########.fr       */
+/*   Updated: 2024/11/25 12:50:42 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,37 @@
 int main(int argc, char **argv) {
 	char *input;
 
-	printf("ARGC: %d, ARGV: %s\n", argc, argv[0]);
+	printf("ARGC: %d, ARGV: %s\n", argc, argv[0]); // added this to avoid compilation blocks as we don't use this yet
 	while (1)
 	{
 		input = readline(COLOR_GREEN "Minishell> " COLOR_RESET);
 		if (strlen(input) > 0)
-			add_history(input); // FOUND BUGS HERE
+			add_history(input); // NOTE FOR LATER - FOUND BUGS WHEN NAVIGATING THROUGH HISTORY
 		if(ft_strcmp(input, "exit") == 0)
 		{
-			free(input);
+			free(input); // REMEMBER - this will need to also be handled in signal handling
 			clear_history();
-			printf("Exiting Minishell..\n");
+			printf("Exiting Minishell..\n");//
 			exit (0);
 		}
-		// Creates mock tokens, this will have to be substituted by the token list created from alejo
-		t_token_list	*tokens = create_mock_tokens(input);
-		//We have our node list! I do not call it AST because AST is a lists of nodes unified by &&, || and ; which is bomus work
-		t_node *node_list = parse(tokens);
-		free_mock_tokens(tokens); // we free the token list after we used them for the nodes
-		free(input);
+		t_token_list	*tokens = create_mock_tokens(input); // <-------------- this needs to be substituted by Alejo's token list
+		t_node *node_list = parse(tokens); //
+		free_mock_tokens(tokens); // we free the token list since we allocated and created our nodes
+		free(input); // we also free the input as we have processed it already as tokens and then nodes
 		if(!node_list)
 		{
-			printf("Error increating node! (I could possibly return the error here too..?)\n"); // free not needed?
-			// actually i can free here at every error returned instead!
+			// planning that every error (not only allocation error but also parsing error) makes the parsing list "NULL"
+			// so that we just need to free once in here and not on every parsing considion with meet
+			// could also return an error, but need to check
+			printf("Error increating node! (I could possibly return the error here too..?)\n");
+			// free_node_list(node_list); // for now this is seen as double free because I al already freeing in every scenario i find in my parsing function
 		}
 		else
 		{
-			print_nodes(node_list); // i am able to print the nodes data here!
-			exec_node(node_list);
-			free_node_list(node_list);// we have to free the node_list before we free the input and get more input
+			// print node logic will be added here just to help us with error handling, we can comment it our or remove it once we submit the project
+			print_nodes(node_list); // let's leave this function for error handling, we can comment it our or remove it once we submit the project
+			exec_node(node_list); // I already set a simple ground for initiating execution from our nodes
+			free_node_list(node_list);// we have to free the node_list when finished executing it and ready to prompt a new input
 		}
 	}
 	clear_history();
