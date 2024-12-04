c9b0b525748416d4c19e56010748ec4b2d6ad39c
Alejandro Ramirez <faramirezs@gmail.com>
2024-12-04 17:49:36 +0100

    Remove unused variables and improve code readability in main function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alramire <alramire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:26:03 by jslusark          #+#    #+#             */
-/*   Updated: 2024/12/04 07:48:03 by alramire         ###   ########.fr       */
+/*   Updated: 2024/12/04 16:48:38 by alramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@
 int	main(int argc, char **argv, char **env)
 {
 	char			*input;
-	char			**env_copy;
-	int				count;
+	//char			**env_copy;
+	//int				count;
 	t_token_list	*tokens;
 	t_node			*node_list;
 	t_char_itr		itr;
 	t_scanner		scanner;
 	(void)argc; // this silences compilation warnings when we do not use argc
 	(void)argv; // this silences compilation warnings when we do not use argv
+	(void)env;
 
-	while
+
 
 	while (1)
 	{
--- parsing/tokenization/token_list.c
+++ parsing/tokenization/token_list.c
@@ -4,12 +4,12 @@
 t_token_list *init_token_list(t_scanner *scanner)
 {
 	t_token_list *new_token;
-	t_token_list *head;    // Head of the linked list
+	t_token_list *head;
 	t_token_list *current;
 
 	head = NULL;
 	current = NULL;
-	while (scanner_has_next(scanner))
+	while (scanner_has_next(scanner) && scanner->next.type != END)
 	{
 		scanner->next = scanner_next(scanner);
 		new_token = OOM_GUARD(malloc(sizeof(t_token_list)), __FILE__, __LINE__);
@@ -17,11 +17,11 @@ t_token_list *init_token_list(t_scanner *scanner)
 		new_token->value = ft_strndup(scanner->next.lexeme.start, scanner->next.lexeme.length);
 		new_token->next = NULL;
 		if (!head)
-			head = new_token; // First token becomes the head
+			head = new_token;
 		else
 			current->next = new_token;
 		current = new_token;
-		//printf("Token Type: %d, Token Value: %s\n", new_token->type, new_token->value);
+		printf("Token Type: %d, Token Value: %s\n", new_token->type, new_token->value);
 		//print_token(scanner->next);
 	}
 	return (head); // Return the head of the linked list
