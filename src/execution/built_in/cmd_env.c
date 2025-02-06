92a5c7efefad18d536d5da7118a420bd25a91b0f
JSlusark <slusark.jessica@gmail.com>
2025-02-06 18:41:41 +0100

    CMD_ENV FIXED

--- src/execution/built_in/cmd_env.c
+++ src/execution/built_in/cmd_env.c
@@ -6,7 +6,7 @@
 /*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:26:33 by stdi-pum          #+#    #+#             */
-/*   Updated: 2025/02/06 18:00:02 by jslusark         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:40:26 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	handle_env(t_node_list *node)
 	{
 		ft_putstr_fd(node->msh->ms_env[i], 1);
 		ft_putstr_fd("\n", 1);
+		i++;
 	}
 	return (0);
 }
