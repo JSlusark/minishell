1ea27d1592b22f047a689dc278dca6e0dadce6c6
JSlusark <slusark.jessica@gmail.com>
2025-02-06 18:59:32 +0100

    CMD PWD AND UNSET - norminette ready

--- src/execution/built_in/cmd_pwd.c
+++ src/execution/built_in/cmd_pwd.c
@@ -6,51 +6,29 @@
 /*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:26:41 by stdi-pum          #+#    #+#             */
-/*   Updated: 2025/01/31 13:15:01 by jslusark         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:42:49 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-int    handle_pwd(t_node_list *node)
+int	handle_pwd(t_node_list *node)
 {
-	char *cwd;
+	char	*cwd;
 
-	cwd = getcwd(NULL, 0); // gets current working directory and allocates the necessary memory dynamically
+	cwd = getcwd(NULL, 0);
 	if (cwd != NULL)
-    {
+	{
 		write(node->fd_out, cwd, ft_strlen(cwd));
-		write(node->fd_out, "\n", 1); // Write a newline at the end
-		free(cwd); // Free allocated memory
-        return (0);
-    }
-	else // commented the other ifs as I am unsure if we have to do this
-    {
+		write(node->fd_out, "\n", 1);
+		free(cwd);
+		return (0);
+	}
+	else
+	{
 		printf(COLOR_GREEN"Minishell> "COLOR_RESET);
-		// if (errno == ERANGE)
-		// {
-		// 	printf("pwd: Buffer size too small\n");
-		// 	node_l->msh->exit_code = 1;
-		// }
-		// else if (errno == EACCES)
-		// {
-		// 	printf("pwd: Permission denied\n");
-		// 	node_l->msh->exit_code = 126;
-		// }
-		// else if (errno == ENOENT)
-		// {
-		// 	printf("pwd: Current directory is inaccessible or deleted\n");
-		// 	node_l->msh->exit_code = 127;
-		// }
-		// else if (errno == ENOMEM)
-		// {
-		// 	printf("pwd: Insufficient memory to allocate path\n");
-		// 	node_l->msh->exit_code = 1;
-		// }
-		// else
-		// {
-			perror("pwd");
-			node->msh->exit_code = 1; // Generic error
-        return (1);
-    }
+		perror("pwd");
+		node->msh->exit_code = 1;
+		return (1);
+	}
 }
--- src/execution/built_in/cmd_unset.c
+++ src/execution/built_in/cmd_unset.c
@@ -1,82 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_unset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jslusark <jslusark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 18:43:11 by jslusark          #+#    #+#             */
+/*   Updated: 2025/02/06 18:59:01 by jslusark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../include/minishell.h"
 
-
-static int find_env_index(char **env, char *var)
+static int	find_env_index(char **env, char *var)
 {
-    int     i;
-    char    **split;
+	int		i;
+	char	**split;
 
-    i = 0;
-    while (env[i])
-    {
-        split = ft_split(env[i], '=');
-        if (ft_strcmp(split[0], var) == 0)
-        {
-            ft_free_tab(split);
-            return i;
-        }
-        ft_free_tab(split);
-        i++;
-    }
-    return -1;
+	i = 0;
+	while (env[i])
+	{
+		split = ft_split(env[i], '=');
+		if (ft_strcmp(split[0], var) == 0)
+		{
+			ft_free_tab(split);
+			return (i);
+		}
+		ft_free_tab(split);
+		i++;
+	}
+	return (-1);
 }
 
 int	ms_remove_line(t_msh *msh, int index)
 {
-    int     i;
-    int     j;
-    char    **new_matrix;
+	int		i;
+	int		j;
+	char	**new_matrix;
 
-    i = 0;
-    while (msh->ms_env[i])
-        i++;
-    new_matrix = malloc(sizeof(char *) * i);
-    if (!new_matrix)
-        return 1;
-    i = 0;
-    j = 0;
-    while (msh->ms_env[i])
-    {
-        if (i != index)
-        {
-            new_matrix[j] = ft_strdup(msh->ms_env[i]);
-            j++;
-        }
-        i++;
-    }
-    new_matrix[j] = NULL;
-    ft_free_tab(msh->ms_env);
-    msh->ms_env = new_matrix;
-	return 0;
+	i = 0;
+	while (msh->ms_env[i])
+		i++;
+	new_matrix = malloc(sizeof(char *) * i);
+	if (!new_matrix)
+		return (1);
+	i = 0;
+	j = 0;
+	while (msh->ms_env[i])
+	{
+		if (i != index)
+		{
+			new_matrix[j] = ft_strdup(msh->ms_env[i]);
+			j++;
+		}
+		i++;
+	}
+	new_matrix[j] = NULL;
+	ft_free_tab(msh->ms_env);
+	msh->ms_env = new_matrix;
+	return (0);
 }
 
-void remove_env(char *var, t_node_list *node)
+void	remove_env(char *var, t_node_list *node)
 {
-    int index;
+	int	index;
 
-    index = find_env_index(node->msh->ms_env, var);
-    if (index != -1)
-    {
-        ms_remove_line(node->msh, index);
-    }
+	index = find_env_index(node->msh->ms_env, var);
+	if (index != -1)
+	{
+		ms_remove_line(node->msh, index);
+	}
 }
 
-void    exec_unset (char **av, t_node_list *node)
+void	exec_unset(char **av, t_node_list *node)
 {
-    /*
-        da jess:
-        - quando unset non trova la env var o ha 0 args l'exit code e' sempre 0, perche' semplicemente non succede nulla
-        - unset puo' avere piu' args, rimuove solo le env esistenti
-     */
-    int i;
+	int	i;
 
-    i = 0;
-    if(node->cmd->args) // important to avoid seg fault if args are NULL
-    {
-        while(node->cmd->args[i])
-        {
-            remove_env(av[i], node);
-            i++;
-        }
-    }
+	i = 0;
+	if (node->cmd->args)
+	{
+		while (node->cmd->args[i])
+		{
+			remove_env(av[i], node);
+			i++;
+		}
+	}
 }
