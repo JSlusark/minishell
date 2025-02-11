61cb497bdf79195390efb4be357cda36ea65e654
Stefano Alexander Di Puma <stdi-pum@c3a8c3.42berlin.de>
2025-02-11 17:24:46 +0100

    norm execbuiltin

--- src/execution/built_in/exec_builtin.c
+++ src/execution/built_in/exec_builtin.c
@@ -6,38 +6,34 @@
 /*   By: stdi-pum <stdi-pum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:25:47 by stdi-pum          #+#    #+#             */
-/*   Updated: 2025/02/11 14:51:25 by stdi-pum         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:24:19 by stdi-pum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-int	exec_builtin_2(t_node_list *node_list)
+void	exec_builtin_two(t_node_list *node_list, int *exit)
 {
-	int	exit;
-
-	exit = 1;
 	if (strcmp(node_list->cmd->cmd, "unset") == 0)
 	{
-		exit = 0;
+		*exit = 0;
 		exec_unset(node_list->cmd->args, node_list);
 	}
-	if (strcmp(node_list->cmd->cmd, "env") == 0)
+	else if (strcmp(node_list->cmd->cmd, "env") == 0)
 	{
-		exit = 0;
+		*exit = 0;
 		handle_env(node_list);
 	}
 	else if (strcmp(node_list->cmd->cmd, "exit") == 0)
 	{
-		exit = 0;
+		*exit = 0;
 		exec_exit(node_list);
 	}
 	else if (strcmp(node_list->cmd->cmd, "export") == 0)
 	{
-		exit = 0;
+		*exit = 0;
 		exec_export(node_list->cmd->args, node_list);
 	}
-	return (exit);
 }
 
 int	exec_builtin(t_node_list	*node_list)
@@ -47,7 +43,7 @@ int	exec_builtin(t_node_list	*node_list)
 	exit = 1;
 	if (!node_list->cmd)
 		return (exit);
-	if ((strcmp(node_list->cmd->cmd, "echo")) == 0)
+	else if (strcmp(node_list->cmd->cmd, "echo") == 0)
 		exit = handle_echo(node_list);
 	else if ((strcmp(node_list->cmd->cmd, "cd")) == 0)
 	{
@@ -59,6 +55,6 @@ int	exec_builtin(t_node_list	*node_list)
 		exit = 0;
 		handle_pwd(node_list);
 	}
-	exit = exec_builtin_2(node_list);
+	exec_builtin_two(node_list, &exit);
 	return (exit);
 }
